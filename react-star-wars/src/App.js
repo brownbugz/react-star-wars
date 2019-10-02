6d9f03c6ee9af4de32087e35d3b73af1167d95e5
sonniakemmer <brownbugz@gmail.com>
2019-10-02 16:14:29 -0700

    adds componentDidMount and adds styling in App.css

--- react-star-wars/src/App.js
+++ react-star-wars/src/App.js
@@ -1,32 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {Route, Switch, Link} from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
+import { getAllStarships } from './services/sw-api';
 import StarshipPage from './pages/StarshipPage/StarshipPage';
 
 class App extends Component {
+  
   state = {
     starships: []
   };
 
-  getStarship(idx) {
-    return this.state.starships(idx);
+  getStarship = (idx) => {
+    return this.state.starships[idx];
   }
 
+  async componentDidMount() {
+    const starships = await getAllStarships();
+    this.setState({ starships: starships.results });
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          STAR WARS STARSHIPS
-        </header>
+        <header className="App-header">STAR WARS STARSHIPS</header>
         <Switch>
-          <Route exact path="/" render={() => 
+          <Route exact path='/' render={() => 
             <section>
-              {this.state.starships.map((starship, idx) =>
+              {this.state.starships.map((starship, idx) => 
                 <Link
-                  to={`/starship/${idx}`}
+                  to={`/starships/${idx}`}
                   key={starship.name}
                 >
-                {starship.name}
+                  {starship.name}
                 </Link>
               )}
             </section>
@@ -38,10 +43,10 @@ class App extends Component {
             />
           }/>
         </Switch>
-
       </div>
     );
   }
+
 }
 
 export default App;
